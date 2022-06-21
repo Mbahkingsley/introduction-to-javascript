7a1be4a0359717769811e400d13c609d7423b4f1
Mbahkingsley <69054532+Mbahkingsley@users.noreply.github.com>
2022-06-21 13:47:35 -0700

    Delete simple calculator.js

--- simple calculator.js
+++
@@ -1,9 +0,0 @@
-var my_name = "Mbah kingsley chinonso";
-var height =  70
-var country = "Nigeria"
-
-document.write("my name is " + my_name);
-document.write("<br>")
-document.write("my height is " + height +"cm");
-document.write("<br>")
-document.write("I am from " + country);
