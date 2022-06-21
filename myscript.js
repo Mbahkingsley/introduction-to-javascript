1b34cc82c055a78dd287cc6fc0ed5d2cbb00faa4
mbah kingsley <mbahkingsley9@gmail.com>
2022-06-21 00:02:02 -0700

    introduction to javascript task 1

---
+++ myscript.js
@@ -0,0 +1,9 @@
+var my_name = "Mbah kingsley chinonso";
+var height =  70
+var country = "Nigeria"
+
+document.write("my name is " + my_name);
+document.write("<br>")
+document.write("my height is " + height +"cm");
+document.write("<br>")
+document.write("I am from " + country);
