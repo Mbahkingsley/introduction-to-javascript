b1c4725811d31b415c8295d2c4339ecada30032c
Mbahkingsley <69054532+Mbahkingsley@users.noreply.github.com>
2022-06-22 10:27:50 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,15 @@
+// A program to print name, height and country
+
+var my_name = "Mbah kingsley chinonso";
+
+var height = 70;
+
+var country = "Nigeria";
+
+document.write("my name is " + my_name);
+
+document.write("<br>");
+
+document.write("my height is " + height + "cm <br>");
+
+document.write("I am from " + country);
